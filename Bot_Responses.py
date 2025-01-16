1b1e93a13db16ddada8d925e9708a5d286be1646
ATLRedRanger <vincentshinholster@gmail.com>
2025-01-15 19:08:28 -0500

    Added new commands.

--- Bot_Responses.py
+++ Bot_Responses.py
@@ -8,7 +8,12 @@ im = Image.open(file)
 def get_response(user_input):
     lowered = user_input.lower()
     #randomNum = 
-    commandsList = ["roll", "hello", "commands", "trigid", "invtrigid", "rps"] # "attack", ]
+    commandsList = ["roll", "hello", "commands", "trigid", "invtrigid", "rps", "intro"] # "attack", ]
+    icebreakers = ["Would you rather have invisibility or flight?\n", "If you could bring back any fashion trend, what would it be?\n",
+                    "Coffee or tea?\n", "What's your favorite breakfast cereal?\n", "What sport would you compete in if you were in the Olympics?\n",
+                      "What is your favorite TV show?\n", "What two things do you consider yourself to be very good at?\n",
+                        "What is your favorite movie?\n", "What is your favorite book?\n", "What movie can you rewatch over and over again?\n", "What's your dream car?\n",
+                        "What's your favorite color?\n", "What's your favorite video game?\n", "Who's your favorite music artist?\n"]
     rps = ["Rock", "Paper", "Scissors"]
     if lowered == "":
         return "Well, you're awfully silent..."
@@ -34,6 +39,10 @@ def get_response(user_input):
             return temp
     elif "google" in lowered:
         return "https://www.google.com"
+    elif "intro" in lowered:
+        introQs = random.choices(icebreakers, k = 3)
+        questions = introQs[0] + introQs[2] + introQs[1]
+        return questions
     else:
         return random.choice(["Does not compute...", "I don't understand.", "Did you mean to say 'roll'?"])
 
